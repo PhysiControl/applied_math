2f766a6071557a3ae76a283d19d2483686232593
Ibrahim Y. <ibrahim.yalcinsoy.tum@gmail.com>
2025-03-10 09:21:19 +0100

    legendre script

---
+++ python/legendre.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as pit
+from scipy.special import legendre
+
+N = 1000
+xvals = np.linspace(-1,1,N)
+
+def Legendre(x,n):
+    leg = legendre(n)
+    P_n = leg(x)
+    return P_n
+
+for i in range(1,16):
+    func = Legendre(xvals, i)
+    plt.plot(xvals,func,label = "n = " + str(i))
+    
+
+plt.title("First 3 P_n(x)")
+plt.grid()
+plt.legend()
+plt.show()
