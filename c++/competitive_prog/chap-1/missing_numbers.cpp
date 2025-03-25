1bae30837f75857d2c7e6966941c89f45e9ede1d
Ibrahim Yalcinsoy <ibrahimyalcinsoy@Ibrahims-MacBook-Pro.local>
2025-03-25 20:38:32 +0100

    second try

--- c++/competitive_prog/chap-1/missing_numbers.cpp
+++ c++/competitive_prog/chap-1/missing_numbers.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
-using namespace std; 
 
-int main(){
-    int n;
-    cin >> n; if(n <= 1) {cout<<"choose n bigger than 1"; return -1;}
-    int l = n-1; 
-    int ar[l-1]; 
-    int b; 
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n <= 1) {
+    cout << "choose n bigger than 1";
+    return -1;
+  }
+  int l = n - 1;
+  int ar[l - 1];
+  int b;
+
+  for (int i = 0; i < l; i++) {
+    cin >> b;
+
+    for (int c = 0; c <= i; c++) {
+      if (ar[c] != b){
+        ar[i] = b;
+        break;
+      }
+      else if (ar[c] == b) {
+        ar[i] = 0;
+        cout << "already typed in, choose another variable!";
+        --i;
+        break;
+      }
+    }
+    cout << ar[i] << " ";
+  }
+
+  return 0;
 }
-
-
-    
-  
